5a0c5cb2e4349f93c84c703a0c1110a66579a34d
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2025-12-30 11:14:18 +0700

    chore: update service defaults IAM extensions

--- Maliev.Aspire.ServiceDefaults/Extensions.IAM.cs
+++ Maliev.Aspire.ServiceDefaults/Extensions.IAM.cs
@@ -50,7 +50,7 @@ public static class IAMExtensions
     public static IServiceCollection AddPermissionAuthorization(this IServiceCollection services)
     {
         services.AddHttpContextAccessor();
-        
+
         // Register authorization services first
         services.AddAuthorization();
 
